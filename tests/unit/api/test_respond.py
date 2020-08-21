314dd58c54b3e604fb1c17714643139f01bb8005
AlinaBlahun <ablahun@cisco.com>
2020-08-21 11:54:16 +0300

    CCTRI-1475: Clean up tests.

--- tests/unit/api/test_respond.py
+++ tests/unit/api/test_respond.py
@@ -7,7 +7,7 @@ from requests import Session
 
 from api.errors import INVALID_ARGUMENT
 from api.respond import ADD_ACTION_ID, REMOVE_ACTION_ID
-from .utils import headers
+from .utils import headers, check_akamai_request
 
 
 def routes():
@@ -38,6 +38,7 @@ def test_respond_call_with_invalid_jwt_failure(
     response = client.post(
         route, headers=headers(invalid_jwt), json=valid_json
     )
+
     assert response.status_code == HTTPStatus.OK
     assert response.json == invalid_jwt_expected_payload
 
@@ -56,9 +57,10 @@ def invalid_json(route):
 @fixture(scope='module')
 def invalid_json_expected_payload(route):
     message = None
+    data = {}
+
     if route.endswith('/observables'):
         message = '{"0": {"value": ["Missing data for required field."]}}'
-        data = {}
     if route.endswith('/trigger'):
         message = '{"action-id": ["Missing data for required field."]}'
         data = {'status': 'failure'}
@@ -77,33 +79,31 @@ def invalid_json_expected_payload(route):
 def test_respond_call_with_valid_jwt_but_invalid_json_failure(
         route, client, valid_jwt, invalid_json, invalid_json_expected_payload
 ):
-    response = client.post(route,
-                           headers=headers(valid_jwt),
-                           json=invalid_json)
+    response = client.post(
+        route, headers=headers(valid_jwt), json=invalid_json
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json == invalid_json_expected_payload
 
 
 def test_respond_observables_call_success(
-        client, valid_jwt,
-        akamai_response_network_lists, respond_observables_expected_payload
+        client, valid_jwt, akamai_response_network_lists,
+        respond_observables_expected_payload
 ):
     with patch.object(Session, 'request') as request_mock:
         request_mock.return_value = akamai_response_network_lists
+
         response = client.post(
             '/respond/observables', headers=headers(valid_jwt),
             json=[{'type': 'ip', 'value': '1.1.1.1'},
                   {'type': 'domain', 'value': 'cisco.com'}]
         )
+
         assert response.status_code == HTTPStatus.OK
         assert response.json == respond_observables_expected_payload
-        request_mock.assert_called_once_with(
-            'GET', 'https://xxx/network-list/v2/network-lists',
-            headers={'Accept': 'application/json',
-                     'User-Agent': 'Cisco Threat Response Integrations'
-                                   ' <tr-integrations-support@cisco.com>'},
-            params={'listType': 'IP', 'includeElements': True}
+        check_akamai_request(
+            request_mock,  {'listType': 'IP', 'includeElements': True}
         )
 
 
@@ -111,20 +111,19 @@ def input_sets():
     input_json = {'observable_type': 'ip',
                   'observable_value': '1.1.1.1',
                   'network_list_id': 'nli'}
+    uri = 'https://xxx/network-list/v2/network-lists/nli/elements'
+    params = {'element': '1.1.1.1'}
     expected_response_success = {'data': {'status': 'success'}}
 
-    TestData = namedtuple('TestData',
-                          'test_name json expected_response request_check')
+    TestData = namedtuple(
+        'TestData', 'test_name json expected_response request_check'
+    )
     yield TestData(
         'add_action_success',
         {**input_json, 'action-id': ADD_ACTION_ID},
         expected_response_success,
-        lambda r: r.assert_called_once_with(
-            'PUT', 'https://xxx/network-list/v2/network-lists/nli/elements',
-            headers={'Accept': 'application/json',
-                     'User-Agent': 'Cisco Threat Response Integrations'
-                                   ' <tr-integrations-support@cisco.com>'},
-            params={'element': '1.1.1.1'}
+        lambda request_mock: check_akamai_request(
+            request_mock,  params, method='PUT', uri=uri
         )
     )
 
@@ -132,21 +131,22 @@ def input_sets():
         'remove_action_success',
         {**input_json, 'action-id': REMOVE_ACTION_ID},
         expected_response_success,
-        lambda r: r.assert_called_once_with(
-            'DELETE', 'https://xxx/network-list/v2/network-lists/nli/elements',
-            headers={'Accept': 'application/json',
-                     'User-Agent': 'Cisco Threat Response Integrations'
-                                   ' <tr-integrations-support@cisco.com>'},
-            params={'element': '1.1.1.1'}
-        ))
+        lambda request_mock: check_akamai_request(
+            request_mock, params, method='DELETE', uri=uri
+        )
+    )
+
     yield TestData(
         'unsupported_action_failure',
         {**input_json, 'action-id': 'unknown'},
         {
             'data': {'status': 'failure'},
-            'errors': [{'code': 'invalid argument',
-                        'message': 'Unsupported action.',
-                        'type': 'fatal'}]},
+            'errors': [
+                {'code': INVALID_ARGUMENT,
+                 'message': 'Unsupported action.',
+                 'type': 'fatal'}
+            ]
+        },
         lambda r: r.assert_not_called()
     )
 
@@ -156,12 +156,12 @@ def input_data(request):
     return request.param
 
 
-def test_respond_trigger(input_data,
-                         client, valid_jwt,
-                         akamai_response_ok
-                         ):
+def test_respond_trigger(
+        input_data, client, valid_jwt, akamai_response_ok
+):
     with patch.object(Session, 'request') as request_mock:
         request_mock.return_value = akamai_response_ok
+
         response = client.post(
             '/respond/trigger', headers=headers(valid_jwt),
             json=input_data.json
@@ -169,5 +169,4 @@ def test_respond_trigger(input_data,
 
         assert response.status_code == HTTPStatus.OK
         assert response.json == input_data.expected_response
-
         input_data.request_check(request_mock)
